3ac5342c1e238d3b5a81a8f5d008a33e2e94b7e1
TatoStw <tatostw5@gmail.com>
2024-11-04 23:33:02 +0100

    Add files via upload
    Vistas

---
+++ TrabajoSGE1/TrabajoSGE1/WindowAddSocio.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrabajoSGE1
+{
+    public partial class WindowAddSocio : Window
+    {
+        private String usuarioLog;
+        private String rangoUsuario;
+        public WindowAddSocio(string usuLog, string rangoUsu)
+        {
+            InitializeComponent();
+            usuarioLog = usuLog;
+            rangoUsuario = rangoUsu;
+            lbl_usuLog.Content = usuarioLog;
+            lbl_rango.Content = rangoUsuario;
+        }
+    }
+}
---
+++ TrabajoSGE1/TrabajoSGE1/WindowRegSocios.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace TrabajoSGE1
+{
+    public partial class WindowRegSocios : Window
+    {
+        private String usuarioLog;
+        private String rangoUsuario;
+
+        public WindowRegSocios(string usuLog, string rangoUsu)
+        {
+            InitializeComponent();
+            usuarioLog = usuLog;
+            rangoUsuario = rangoUsu;
+            lbl_usuLog.Content = usuarioLog;
+            lbl_rango.Content = rangoUsuario;
+
+            List<Socio> lista = new List<Socio>
+            {
+                new Socio()
+                {
+                    Id = 1,
+                    Dni = "09134336R",
+                    Nombre = "Jose",
+                    CuentBanc = "****3546",
+                    Dir = "C/Juan Pollas",
+                    Cuota = "Pagada",
+                    FechAlt = "04/11/2024",
+                    FechCad = "04/12/2024"
+                }
+            };
+
+            lst_socios.ItemsSource = lista;
+        }
+
+        private void btn_volver_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w1 = new Window1(usuarioLog, rangoUsuario);
+            w1.Show();
+            this.Close();
+        }
+
+        public class Socio
+        {
+            public int Id { get; set; }
+            public String Dni { get; set; }
+            public String Nombre { get; set; }
+            public String CuentBanc { get; set; }
+            public String Dir { get; set; }
+            public String Cuota { get; set; }
+            public String FechAlt { get; set; }
+            public String FechCad { get; set; }
+        }
+
+        private void btn_addSocio_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
